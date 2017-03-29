e2c58647833fbdc685dfbe62d50e8b6af4b9d0f6
Valeria de Leon <valery.deleon@gmail.com>
2017-03-29 11:36:03 -0600

    antes de que se rompa :3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,71 @@
-var inputNuevoPendiente = document.getElementById('inputNuevoPendiente');
-var inputNuevoItem = document.getElementById('inputNuevoItem');
+// var inputNuevoPendiente = document.getElementById('inputNuevoPendiente');
+// var nuevaListaDesordenada = document.createElement("ul");
 
-function agregarPendiente(){
-  var agregarTituloPendiente = document.getElementById('tituloPendiente');
-  agregarTituloPendiente.innerText = inputNuevoPendiente.value;
-}
-agregarPendiente()
 
-function agregarlistaDesordenada(){
-  var agregarItemAlistaDesordenada = document.getElementById('item');
-  agregarItemAlistaDesordenada.innerText = inputNuevoItem.value;
+var arregloPendiente = [];
+
+function Pendiente (tituloPendiente, itemPendiente){
+  this.tituloPendiente = tituloPendiente;
+  this.itemPendiente = itemPendiente;
+};
+
+
+function crearPendiente(){
+  var tituloPendiente = document.getElementById('inputNuevoPendiente');
+  var itemPendiente =  document.getElementById('inputNuevoItem');
+
+  var pendiente = new Pendiente(
+    tituloPendiente.value,
+    itemPendiente.value
+  )
+  tituloPendiente.value = "";
+  itemPendiente.value = "";
+
+  arregloPendiente.push(pendiente);
+  mostrarPendiente();
+};
+
+function mostrarPendiente(){
+  listaDesordenada = document.createElement("ul");
+
+  var sectionLista = document.getElementById("pendienteS");
+  sectionLista.innerHTML = "";
+
+  arregloPendiente.forEach(function(pendiente){
+    nuevoLi = document.createElement("li");
+    nuevoLi.innerHTML = pendiente.itemPendiente;
+    listaDesordenada.appendChild(nuevoLi);
+  })
+  sectionLista.appendChild(listaDesordenada);
 }
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+// function agregarlistaDesordenada(){
+//
+//   for(var i = 0; i )
+//
+// }
+//
+//
+//
+//
+// function agregarPendiente(){
+//   var agregarTituloPendiente = document.getElementById('pendienteS');
+//   agregarTituloPendiente.innerHTML = inputNuevoPendiente.value;
+// }
+// agregarPendiente()
+//
+// function agregarlistaDesordenada(){
+//   var agregarItemAlistaDesordenada = document.getElementById('item');
+//   agregarItemAlistaDesordenada.innerText = inputNuevoItem.value;
+// }
